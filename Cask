e9a47064cdaa9347eaf2f3a28f6aaa2a4d81fb7c
Gong Qijian <gongqijian@gmail.com>
2024-11-07 08:54:28 +0800

    Generated by cookiecutter

---
+++ Cask
@@ -0,0 +1,6 @@
+(source melpa)
+
+(package "avyium" "0.0.1" "Vimium like for Emacs based on avy")
+
+(development
+ (depends-on "ert-runner"))
---
+++ avyium.el
@@ -0,0 +1,36 @@
+;;; avyium.el --- Vimium like for Emacs based on avy -*- lexical-binding: t -*-
+
+;; Copyright (C) 2024 Gong Qijian <gongqijian@gmail.com>
+
+;; Author: Gong Qijian <gongqijian@gmail.com>
+;; Created: 2024/11/07
+;; Homepage: https://github.com/twlz0ne/avyium
+;; Keywords: tools
+;; Package-Requires: ((emacs "25.1")))
+;; Version: 0.0.1
+
+;; This file is not part of GNU Emacs
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Vimium like for Emacs based on avy
+
+;;; Code:
+
+
+(provide 'avyium)
+
+;;; avyium.el ends here
